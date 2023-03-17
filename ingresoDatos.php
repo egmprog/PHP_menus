aa90b7399187d15237b6f32d428a6eb5a5996ce8
egmprog <egmprog@gmail.com>
2023-03-17 12:09:40 -0300

    20230317

---
+++ ingresoDatos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+<body>
+
+<form action="menu.php" method="post">
+
+<p>Lugar de salida: </p>
+  <input type="radio" id="dato1" name="dato1" value="Lugar 1">
+  <label for="html">Lugar 1</label><br>
+  <input type="radio" id="css" name="dato1" value="Lugar 2">
+  <label for="css">Lugar 2</label><br>
+
+<p>Lugar de destino: </p>
+  <input type="radio" id="html" name="dato2" value="Lugar 3">
+  <label for="html">Lugar 3</label><br>
+  <input type="radio" id="css" name="dato2" value="Lugar 4">
+  <label for="css">Lugar 4</label><br>
+  <input type="submit" value="Enviar">
+  <input type="reset" value="Borrar las opciones seleccionadas" />
+</form>
+
+
+</body>
+</html>
